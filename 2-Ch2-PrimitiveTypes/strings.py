984fb9bbf1a6e982fc1977c3f3cf898ef0a48822
Balasubramanian Nainar <77400713+nainarbala@users.noreply.github.com>
2025-07-07 04:43:40 +0000

    python course ch2

---
+++ 2-Ch2-PrimitiveTypes/strings.py
@@ -0,0 +1,13 @@
+course = "Python Programming"
+
+print(len(course))
+print(course[0])
+print(course[-17])
+print(course[-18])
+
+count = len(course)+10
+print(len(course)+10)
+print(course[0:28])
+
+course1 = "Pythin \\Test"
+print(course1)
---
+++ 2-Ch2-PrimitiveTypes/variables.py
@@ -0,0 +1,5 @@
+student_count = 1000
+rating = 4.99
+is_pulished = False
+course_name = "Python program"
+print(student_count, rating, is_pulished, course_name)
