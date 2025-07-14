d25aaf0690fd243c1487d4f34d06cc8e9625d5e0
Balasubramanian Nainar <77400713+nainarbala@users.noreply.github.com>
2025-07-14 04:45:37 +0000

    zip changes

---
+++ 9-Ch9-StandardLibraries/zip.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from zipfile import ZipFile
+
+path = Path();
+
+# with ZipFile("files.zip", "w") as zip:
+#     for p in path.rglob("*.*"):
+#         zip.write(p)
+
+
+# with ZipFile("files.zip", "r") as zip:
+#     print(zip.namelist())
+
+
+
