78b376c1e0500190aba000cbe9894771b697e70a
balasubramanian nainar <nainarbala@gmail.com>
2026-08-21 07:52:44 +0530

    chapter 2

---
+++ 2-Ch2-PrimitiveTypes/student.py
@@ -0,0 +1,16 @@
+"""Student """
+
+student_count = 100
+is_published = False
+student_rating = 4.99
+address = "Adress"
+
+print(type(student_count))
+print(type(is_published))
+print(type(student_rating))
+print(type(address))
+
+# <class 'int'>
+# <class 'float'>
+# <class 'bool'>
+# <class 'str'>
