47e8b0c3285d0bb832ee9a14850e67c6db7e2e2b
balasubramanian nainar <nainarbala@gmail.com>
2026-08-25 09:43:09 +0530

    type changels

---
+++ 2-Ch2-PrimitiveTypes/typeconversion.py
@@ -0,0 +1,17 @@
+# x = input("x:")
+# y = x + 1
+# print(1 + "10")
+
+
+# int(x)
+# float(x)
+# bool(x)
+# str(x)
+
+x = input("x:")
+y = int(x) + 1
+print(f"x: {x}, y:{y}")
+print(bool(0))
+print(bool(1))
+print(bool(""))
+print(bool(None))
