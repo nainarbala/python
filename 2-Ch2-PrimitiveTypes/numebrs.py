66439f972fdb94cfbda7efb3255c1fd419e1931c
balasubramanian nainar <nainarbala@gmail.com>
2026-08-25 08:17:03 +0530

    numbers

---
+++ 2-Ch2-PrimitiveTypes/numebrs.py
@@ -0,0 +1,18 @@
+x = 1
+y = 1.2
+z = 1 + 2j
+print(x)
+print(y)
+print(z)
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+
+x += 10
+
+print(x)
