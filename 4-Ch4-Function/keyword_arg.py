cf3ea2dd86f44a154b0d58547e42ca467d5227a0
balasubramanian nainar <nainarbala@gmail.com>
2026-08-26 19:05:42 +0530

    function

---
+++ 4-Ch4-Function/keyword_arg.py
@@ -0,0 +1,6 @@
+def increment(number, by):
+    return number + by
+
+
+print(increment(10, 3))
+print(increment(number=10, by=20))
---
+++ 4-Ch4-Function/types_of_function.py
@@ -0,0 +1,10 @@
+def grett(name):
+    print(f"Hi {name}")
+
+
+def get_greetings(name):
+    return f"Hi {name}"
+
+
+print(grett("test"))
+print(get_greetings("rest"))
