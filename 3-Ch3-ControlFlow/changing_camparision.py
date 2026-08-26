505bd43f515a306c1f43e475afcca08c2756c2f3
balasubramanian nainar <nainarbala@gmail.com>
2026-08-26 14:16:44 +0530

    chnage comparision

---
+++ 3-Ch3-ControlFlow/changing_camparision.py
@@ -0,0 +1,14 @@
+# age should be between 18 and 65
+
+age = 65
+
+if age >= 18 and age < 65:
+    print("Eligible")
+else:
+    print("Not eligible")
+
+
+if 18 <= age < 65:
+    print("Eligible")
+else:
+    print("Not eligible")
