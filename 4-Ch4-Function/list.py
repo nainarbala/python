5522de2da8ce86576236132a55930ee8a9ef667e
balasubramanian nainar <nainarbala@gmail.com>
2026-08-27 07:14:45 +0530

    ds

---
+++ 4-Ch4-Function/list.py
@@ -0,0 +1,12 @@
+letters = ["a", "b", "c"]
+matrix = [[2, 3, 4], [3, 1, 4], [4, 6, 7]]
+zeros = [10] * 5
+combined = zeros + letters
+numebrs = list(range(10))
+chars = list("Python programming")
+
+print(type(letters))
+print(type(matrix))
+print(matrix)
+print(combined)
+print(chars)
