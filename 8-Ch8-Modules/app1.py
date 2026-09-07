81be9c6fe0ba6fdeeb37f1f07064938719c75c8a
balasubramanian nainar <nainarbala@gmail.com>
2026-09-07 18:23:38 +0530

    pakcages

--- 8-Ch8-Modules/app1.py
+++ 8-Ch8-Modules/app1.py
@@ -1,5 +1,6 @@
-from ecommerce1.sale2 import shipping
-from ecommerce1 import sale2
+from ecommerce1.shopping import sale2
+from ecommerce1.shopping import sale2
+from ecommerce1.shopping.sale2 import shipping
 
 shipping()
 sale2.shipping()
